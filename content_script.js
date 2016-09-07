1e2558143746aca46e2e77ee8bdd1706f2c84d7f
Mohd.Monis <mohd.monis@finoit.co.in>
2016-09-07 18:17:12 +0530

    complete file setup

---
+++ content_script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    var time, x = [];
+    if ($('.estimated-hours:last-child').text() != '') {
+        time = parseFloat($('.estimated-hours:last-child').text().substr(0, 4));
+    }
+    var pageInfo = {}
+
+
+    var x = 1;
+    var y= 0;
+
+    /* $('[name="commit"]').each(function() {
+         x.push($(this).val());
+     })*/
+    $('[name="commit"]').click(function() {
+        alert($(this).val());
+        if ($(this).val() == "Add") {
+            //when any new issue is added
+
+        }
+        if ($(this).val() == "Pause") {
+            //when issue is paused
+            console.log("I am paused");
+            var port = chrome.runtime.connect({ name: "knockknock" });
+            port.postMessage({ joke: "Knock knock" });
+            port.onMessage.addListener(function(msg) {
+                if (msg.question == "Who's there?")
+                    port.postMessage({ answer: "Madame" });
+                else if (msg.question == "Madame who?")
+                    port.postMessage({ answer: "Madame... Bovary" });
+            });
+
+        }
+        if ($(this).val() == "Resume") {
+            //when issue is resume
+            console.log("I am resumed");
+        }
+        if ($(this).val() == "Resolve to Review") {
+            //when issue is Resolved
+        }
+
+        if ($(this).val() == "Submit") {
+
+        }
+    })
+    pageInfo["estimatedTime"] = time;
+    pageInfo["author"] = $('.author>a:first-child').text();
+    pageInfo["status"] = 'Active';
+    pageInfo['startDate'] = $('.start-date:last-child').text();
+    pageInfo['startDate'] = $('.due-date:last-child').text();
+    pageInfo['priority'] = $('.priority:last-child').text();
+    pageInfo["issueName"] = $('.subject>div>h3').text();
+    //temporary
+    pageInfo["bugNumber"] = $('h2').text();
+
+    // console.log(pageInfo);
+   /* while (true) {
+        x++;
+
+        if (x == 9000) {
+            chrome.runtime.sendMessage({ Message: x})
+            y++;
+        }
+        if(y==20){
+            break;
+        }
+    }*/
+
+
+})
---
+++ popup.js
@@ -0,0 +1,67 @@
+//on-document ready
+$(document).ready(function() {
+    var storageValue;
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        console.log(tabs[0].url);
+
+        chrome.storage.local.get("temp", function(val) {
+            storageValue = val.temp;
+            if (storageValue) {
+                $('#awesomeCheckbox').prop('checked', true);
+            } else {
+                $('#awesomeCheckbox').prop('checked', false);
+
+            }
+        })
+
+        //disabeling checkbox [will use later]
+        if (tabs[0].url != 'http://workroom.finoit.com/' && !storageValue) {
+            debugger;
+            $('#awesomeCheckbox').attr("disabled", true);
+            $("#showlink").append("<p><a id='openLink' href='' target='_blank'>click here</a> to start the awesomeness</p>");
+            document.getElementById('openLink').onclick = openMe;
+
+        }
+    });
+
+    document.getElementById('awesomeCheckbox').onclick = testMe;
+
+    function openMe() {
+        // chrome.tabs.create({ url: 'www.google.com' });
+        window.open('www.google.com', '_blank');
+    }
+
+
+
+    function testMe() {
+        var obj1 = {
+            actualVal: true
+        }
+        if ($(this).is(':checked')) {
+
+            chrome.storage.local.set({ "temp": true }, function() {
+
+            })
+
+
+        } else {
+            /*obj1["actualVal"] = false;
+            chrome.storage.sync.set(obj1, function() {
+                // Notify that we saved.
+                console.log('Settings set false');
+            });
+        
+*/
+
+            chrome.storage.local.set({ "temp": false }, function() {
+
+            })
+
+        }
+    }
+
+
+
+
+});
