c11503549944b0c2c21081a2bdde7f3caa93883b
monis01 <mnskhn7997@gmail.com>
2016-09-18 16:26:54 +0530

    issue template class created

--- content_script.js
+++ content_script.js
@@ -1,50 +1,16 @@
+//@ for present all is in content script, have to send to background
+
 $(document).ready(function() {
 
     var time, x = [];
+    var pageInfo = {};
+    var allIssues = [];
+
+    //@ estimated time
     if ($('.estimated-hours:last-child').text() != '') {
         time = parseFloat($('.estimated-hours:last-child').text().substr(0, 4));
     }
-    var pageInfo = {}
-
-
-    var x = 1;
-    var y = 0;
-
-    /* $('[name="commit"]').each(function() {
-         x.push($(this).val());
-     })*/
-
-    $('[name="commit"]').click(function() {
-        alert($(this).val());
-        if ($(this).val() == "Add") {
-            //when any new issue is added
-
-        }
-        if ($(this).val() == "Pause") {
-            //when issue is paused
-            console.log("I am paused");
-            var port = chrome.runtime.connect({ name: "knockknock" });
-            port.postMessage({ joke: "Knock knock" });
-            port.onMessage.addListener(function(msg) {
-                if (msg.question == "Who's there?")
-                    port.postMessage({ answer: "Madame" });
-                else if (msg.question == "Madame who?")
-                    port.postMessage({ answer: "Madame... Bovary" });
-            });
-
-        }
-        if ($(this).val() == "Resume") {
-            //when issue is resume
-            console.log("I am resumed");
-        }
-        if ($(this).val() == "Resolve to Review") {
-            //when issue is Resolved
-        }
-
-        if ($(this).val() == "Submit") {
-
-        }
-    })
+    //@ other issue related info
     pageInfo["estimatedTime"] = time;
     pageInfo["author"] = $('.author>a:first-child').text();
     pageInfo["status"] = 'Active';
@@ -55,18 +21,63 @@ $(document).ready(function() {
     //temporary
     pageInfo["bugNumber"] = $('h2').text();
 
-    // console.log(pageInfo);
-    /* while (true) {
-         x++;
+    /* $('[name="commit"]').each(function() {
+        x.push($(this).val());
+    })*/
 
-         if (x == 9000) {
-             chrome.runtime.sendMessage({ Message: x})
-             y++;
-         }
-         if(y==20){
-             break;
-         }
-     }*/
+    $('[name="commit"]').click(function() {
+        alert($(this).val());
+        //when any new issue is added
+        if ($(this).val() == "Add") {
+             //@ checking whether new isse exists (in case of reopening )
+            if (allIssues.indexOf(pageInfo["bugNumber"]) != -1) {
+                allIssues.push(pageInfo["bugNumber"]);
+                chrome.storage.local.set({ "issueList": allIssues }, function() {
+                    console.log('New entry in Issue List ');
+                })
+            }
+            //@ new issue created
+            var newIssue = new issueTemplate(pageInfo);
+        }
+        // when issue is paused
+        if ($(this).val() == "Pause") {
+            console.log("I am paused");
+            //@ sample example of message passing
+            var port = chrome.runtime.connect({ name: "knockknock" });
+            port.postMessage({ joke: "Knock knock" });
+            port.onMessage.addListener(function(msg) {
+            if (msg.question == "Who's there?")
+                port.postMessage({ answer: "Madame" });
+            else if (msg.question == "Madame who?")
+                port.postMessage({ answer: "Madame... Bovary" });
+            });
 
+        }
+        
+        //@ when issue is resume
+        if ($(this).val() == "Resume") {
+            console.log("I am resumed");
+        }
+        //@ when issue is resolved            
+        if ($(this).val() == "Resolve to Review") {
+        
+        }
+
+        if ($(this).val() == "Submit") {
+
+            }
+    }
+
+//@ template for issues
+function issueTemplate(activeIssue) {
+    activeIssue.bugNumber = {
+        this.author: activeIssue.author,
+        this.status: activeIssue.status,
+        this.startDate: activeIssue.startDate,
+        this.priority: activeIssue.priority,
+        this.issueName: activeIssue.issueName,
+        this.estimatedTime: activeIssue.estimatedTime
+    }
+}
 
 })
